17ff878bb5dd0ea3228fd41abf7f7fd8995b778f
Mischa S <revmischa@cpan.org>
2014-02-17 13:57:53 -0800

    chars are one char

--- 5_strings.c
+++ 5_strings.c
@@ -14,13 +14,13 @@ int main() {
   // truncate the string by replacing the second element of the char
   // array with a NULL terminator
   hello[1] = 0;  // same as '\0', 0x00, NULL, etc..
-  
+   
   printf("The character sequence \"%s\" is %i characters long.\n",
          hello, strlen(hello));
 
   // remove all terminating NULLs, this is likely to end in pain and tears
   hello[1] = 'e';
-  hello[5] = '.....';
+  hello[5] = '.'
   puts(hello);
 
   return 0;
